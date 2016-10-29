2cac31cec3e793e98ee1fbb7720e1e2d7d8022ac
Richard Li <li.richard.2013@gmail.com>
2016-10-29 11:37:16 -0400

    initial pathfinding work

---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pathfinding : MonoBehaviour {
+
+	private GameObject player;
+	private NavMeshAgent agent;
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+
+		// NavMeshAgent does pathfinding around the map automatically
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update() {
+
+		// set the ally/enemy's target destination to the player
+		agent.destination = target.transform.position;
+		// needs some work for flocking: arranging neatly and without colliding with each other and the player
+	}
+
+}
