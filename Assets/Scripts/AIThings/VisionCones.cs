caf6a0771d136a1cbffd691b610036b3f76389d2
Richard Li <li.richard.2013@gmail.com>
2016-10-31 00:40:47 -0400

    added basic vision cone functionality

---
+++ Assets/Scripts/AIThings/VisionCones.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionCones : MonoBehaviour {
+
+	public GameObject target;
+
+	void Update () {
+		Vector3 targetDir = target.transform.position - transform.position;
+		float angle = Vector3.Angle(targetDir, transform.forward);
+
+		if(angle < 30.0f)
+			Debug.Log( "close" );
+	}
+}
